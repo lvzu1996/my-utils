be766a44ec288e115991af2373dcece06f20ff3c
lvzu1996 <385162733@qq.com>
2017-10-16 11:09:36 +0800

    增加了cookie操作工具

---
+++ CookieGetter/cookieGetter.js
@@ -0,0 +1,47 @@
+const strCookie = document.cookie;
+const arrCookie = strCookie.split("; ");
+
+//返回一个数组，数组中每个元素是一个长度为2的数组，下标1为key2为value
+function getCookieArr() {
+  var cookieArr = []
+  for (var i = 0; i < arrCookie.length; i++) {
+    var arr = arrCookie[i].split("=");
+    if (arr.length != 2) {
+      var cookieValue = arr.slice(1, arr.length).join()
+      arr = [arr[0],cookieValue]
+    }
+    cookieArr.push(arr)
+  }
+  console.log(cookieArr);
+  return cookieArr
+}
+
+//接收一个cookie的key值，返回对应的value，若未查找到，返回""
+function getCookieValue(name) {
+    var strCookie = document.cookie;
+    var arrCookie = strCookie.split("; ");
+    for (var i = 0; i < arrCookie.length; i++) {
+        var arr = arrCookie[i].split("=");
+        if (arr[0] == name)
+            return arr[1];
+    }
+    return "";
+}
+
+//返回一个key=value的键值对
+function getCookieMap(name) {
+    var strCookie = document.cookie;
+    var arrCookie = strCookie.split("; ");
+    for (var i = 0; i < arrCookie.length; i++) {
+        var arr = arrCookie[i].split("=");
+        if (arr[0] == name)
+            return arr.join('=');
+    }
+    return "";
+}
+
+module.exports = {
+  getCookieArr,
+  getCookieValue,
+  getCookieMap,
+}
